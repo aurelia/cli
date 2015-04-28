cae5e242f2096e78126dab1ce034fa735d422062
joelcoxokc <joel.cox.dev@gmail.com>
2015-04-28 03:01:35 -0500

    refactor(separate-lib-from-cli): Make a clear separation between the cli and the aurelia-cli Library
    Create a cli interface at lib/cli
    Resolve potential symlink issues revolving around linux and windows machines
    Create object on process.AURELIA for linux and windows machines
    Create object on process.env.AURELIA for other machines that do not have symlink issues
    Create process import method for importing local modules
    
    Refactor resolves issue #8

--- bin/aurelia.js
+++ bin/aurelia.js
@@ -3,42 +3,38 @@ const Liftoff = require('liftoff');
 const argv = require('minimist')(process.argv.slice(2));
 
 const AureliaCLI = new Liftoff({
-  name: 'aurelia-cli',
-  configName: 'Aureliafile',
-  extensions: require('interpret').jsVariants,
-  // ^ automatically attempt to require module for any javascript variant
-  // supported by interpret.  e.g. coffee-script / livescript, etc
-  v8flags: ['--harmony'] // to support all flags: require('v8flags')
-    // ^ respawn node with any flag listed here
-}).on('require', function(name, module) {
-  console.log('Loading:', name);
-}).on('requireFail', function(name, err) {
-  console.log('Unable to load:', name, err);
-}).on('respawn', function(flags, child) {
-  console.log('Detected node flags:', flags);
-  console.log('Respawned to PID:', child.pid);
+    name: 'aurelia-cli',
+    configName: 'Aureliafile',
+    extensions: require('interpret').jsVariants,
+    // ^ automatically attempt to require module for any javascript variant
+    // supported by interpret.  e.g. coffee-script / livescript, etc
+    v8flags: ['--harmony'] // to support all flags: require('v8flags')
+      // ^ respawn node with any flag listed here
 });
 
+AureliaCLI
+    .on('require', function(name, module) {
+        console.log('Loading:', name);
+    })
+
+    .on('requireFail', function(name, err) {
+        console.log('Unable to load:', name, err);
+    })
+
+    .on('respawn', function(flags, child) {
+        console.log('Detected node flags:', flags);
+        console.log('Respawned to PID:', child.pid);
+    });
 
 var cli = require('../lib/cli');
 
-cli.init(argv);
-
-AureliaCLI.launch({
+cli.configure({
+  argv: argv,
   cwd: argv.cwd,
   configPath: argv.aureliafile,
   require: argv.require,
   completion: argv.completion,
-  verbose: argv.verbose
-}, invoke);
-
-function invoke(env) {
-
-    cli.configure(env)
-
-    var Aurelia = require(env.modulePath);
-    var aurelia = new Aurelia(env);
-    require(env.configPath)(aurelia);
-
-    cli.ready(aurelia);
-}
+  verbose: argv.verbose,
+  Lyftoff: AureliaCLI,
+  launchFile: 'lib/program.js'
+});
