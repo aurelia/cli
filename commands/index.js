123ad2523e0626c7f44596d92003b3962ced8afb
joelcoxokc <joel.cox.dev@gmail.com>
2015-04-30 05:39:34 -0500

    feat(command-init): Create new Aureliafile on init
    Check if Aureliafile exists
    if Aureliafile exists then prompt for permission to overwrite, otherwise create a new Aurelia file
    Create Aurelia file from existing Aureliafile at cli application's root
    
    feature resolves issue #7

---
+++ commands/index.js
@@ -0,0 +1,26 @@
+var isArg, exists = require('fs').existsSync;
+
+// Command is a service for routing commands to specific files.
+// This service is assuming that the command file is a function..
+module.exports = function Command(cmd) {
+    cmd = (__dirname + '/' + cmd + '.js');
+    // Checking for whether the files exists at this point should be only for development
+    // Otherwise, we know the files exists, so this just become an extra function call
+    // Once fully deployed, we shouldn't be checking whether the command file exists.
+    isArg = exists(cmd);
+
+    // We probably should not be exiting here.
+    // It is best to return here and exit at the root.
+    // This will only be a defect on a Linux machine.
+    // Let's consider making an exiting service that will exit once the commands are done, or on errors.
+    if ( !isArg ) {
+        console.log('[%s] Command %s does not exist!', 'Error'.red, cmd.red);
+        // return;
+        process.ext(1);
+    }
+
+    //return a function for program.action() to call, and pass the arguments through.
+    return function() {
+        return require( cmd ).apply(this, arguments);
+    }
+};
---
+++ commands/init.js
@@ -0,0 +1,47 @@
+var cli    = process.AURELIA;
+var stream = require('vinyl-fs');
+var exists = require('fs').existsSync;
+var logger = cli.import('lib/logger');
+var ask    = cli.import('lib/ask');
+// INIT
+//
+// Executable Command that will initialize the directory, and add an AureliaFile if !exists
+//
+function Init(){
+    logger.ok('initializing');
+    var Aureliafile = process.cwd() + '/Aureliafile.js';
+    exists(Aureliafile)
+        ? prompt()
+            .then(function(answers){
+                answers.overwrite
+                    ? createFile()
+                    : logger.ok('Aureliafile was not overwritten!');
+
+            })
+        : createFile();
+
+};
+
+function createFile(){
+    return stream.src(cli.root('Aureliafile.js'))
+        .pipe(stream.dest(process.cwd()))
+        .on('error', function(err){
+            logger.err('Issue creating %s', 'AureliaFile'.green)
+            console.log(err);
+        })
+        .on('end', function(){
+            logger.ok('%s created', 'AureliaFile'.green);
+        });
+}
+
+function prompt() {
+    var prompts = [{
+        type: 'confirm',
+        name: 'overwrite',
+        message: 'Aureliafile already exists!'.red+' Would you like to overwrite?',
+        default: false
+    }]
+    return ask(prompts);
+}
+
+module.exports = Init;
