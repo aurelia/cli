748a7417149570c76c280dbd816ddbff237278a4
Zewa <zewa666@gmail.com>
2015-05-08 09:06:46 +0200

    feat(generate): generates a viewmodel/view
    This adds the generator feature to create a viewmodel and a view, together with injection handling. The user may see a preview of the file and cancel creation.

--- commands/generate.js
+++ commands/generate.js
@@ -5,10 +5,33 @@ var logger  = cli.import('lib/logger');
 //
 // Executable Command for Generating new file type based on type specified
 function Generate(cmd, options) {
-    logger.log('exec "%s" using %s mode', cmd, options.name());
-    logger.log('Not yet implemented...');
-    logger.log('-----------------------------------');
-    logger.log('  - Generating not yet implemented');
+  if(typeof options.name === 'function' || options.name === '') {
+    logger.err('You must provide a name for the new element');
+    return;
+  }
+
+  var generator = cli.import('lib/generator');
+
+  if(cmd.toLowerCase() === generator.templateType.vm) {
+    var template = options.template || 'default',
+      inject   = options.inject;
+
+    generator.createViewModel(options.name, template, inject).then( function(response) {
+      logger.log(response);
+
+      if(options.view && options.view !== '') {
+        generator.createView(options.name, template)
+          .then(function(response) { logger.log(response); })
+          .catch(function(response) { logger.log(response); });
+      }
+    }).catch(function(err) {
+      logger.log(err);
+    });
+  } else if( cmd.toLowerCase() === generator.templateType.view ) {
+    var template = options.template || 'default';
+
+    generator.createView(options.name, template);
+  }
 }
 
 module.exports = Generate;
