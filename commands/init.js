af111c61e758e3960e9ce548cb0aeabf31ec3014
Patrick Walters <patrickwaltersc21@yahoo.com>
2015-05-09 11:00:50 -0500

    chore(cli): Fix indentation issues in CLI
    Fixes #32

--- commands/init.js
+++ commands/init.js
@@ -8,40 +8,40 @@ var ask    = cli.import('lib/ask');
 // Executable Command that will initialize the directory, and add an AureliaFile if !exists
 //
 function Init(){
-    logger.ok('initializing');
-    var Aureliafile = process.cwd() + '/Aureliafile.js';
-    exists(Aureliafile)
-        ? prompt()
-            .then(function(answers){
-                answers.overwrite
-                    ? createFile()
-                    : logger.ok('Aureliafile was not overwritten!');
+  logger.ok('initializing');
+  var Aureliafile = process.cwd() + '/Aureliafile.js';
+  exists(Aureliafile)
+  ? prompt()
+  .then(function(answers){
+    answers.overwrite
+    ? createFile()
+    : logger.ok('Aureliafile was not overwritten!');
 
-            })
-        : createFile();
+  })
+  : createFile();
 
 };
 
 function createFile(){
-    return stream.src(cli.root('Aureliafile.js'))
-        .pipe(stream.dest(process.cwd()))
-        .on('error', function(err){
-            logger.err('Issue creating %s', 'AureliaFile'.green);
-            console.log(err);
-        })
-        .on('end', function(){
-            logger.ok('%s created', 'AureliaFile'.green);
-        });
+  return stream.src(cli.root('Aureliafile.js'))
+  .pipe(stream.dest(process.cwd()))
+  .on('error', function(err){
+    logger.err('Issue creating %s', 'AureliaFile'.green);
+    console.log(err);
+  })
+  .on('end', function(){
+    logger.ok('%s created', 'AureliaFile'.green);
+  });
 }
 
 function prompt() {
-    var prompts = [{
-        type: 'confirm',
-        name: 'overwrite',
-        message: 'Aureliafile already exists!'.red+' Would you like to overwrite?',
-        default: false
-    }];
-    return ask(prompts);
+  var prompts = [{
+    type: 'confirm',
+    name: 'overwrite',
+    message: 'Aureliafile already exists!'.red+' Would you like to overwrite?',
+    default: false
+  }];
+  return ask(prompts);
 }
 
 module.exports = Init;
--- commands/plugin.js
+++ commands/plugin.js
@@ -5,10 +5,10 @@ var logger  = cli.import('lib/logger');
 //
 // Executable Command for Listing all installed plugins
 function Plugin(cmd, options) {
-    logger.log('exec "%s" using %s mode', cmd, options.name());
-    logger.log('Not yet implemented...');
-    logger.log('-----------------------------------');
-    logger.log('  - Plugin management not yet implemented');
+  logger.log('exec "%s" using %s mode', cmd, options.name());
+  logger.log('Not yet implemented...');
+  logger.log('-----------------------------------');
+  logger.log('  - Plugin management not yet implemented');
 }
 
 module.exports = Plugin;
