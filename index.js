500224c5ab6ca2e218e2aead3d8e6ad3635fe187
joelcoxokc <joel.cox.dev@gmail.com>
2015-05-19 23:06:06 -0700

    chore(semi-colon): Add semi-colons

--- index.js
+++ index.js
@@ -4,16 +4,16 @@ var libDir = path.join.bind(path, __dirname, 'dist', 'lib');
 
 var Api = function() {
   this.logger = require(libDir('logger'));
-}
+};
 
 Api.prototype = {
   get bundler() {
-    return require(libDir('bundler'))
+    return require(libDir('bundler'));
   },
   get installer() {
-    return require(libDir('installer'))
+    return require(libDir('installer'));
   }
-}
+};
 
 module.exports = (function() {
   if (!_api) {
