97a9ce427e9c3d70fd0cecc6548ade09a54cfc0a
joelcoxokc <joel.cox.dev@gmail.com>
2015-05-21 06:59:28 -0700

    chore(Remove-build-dir): Remove existing build Dir

--- build/tasks/build.js
+++ build/tasks/build.js
@@ -14,13 +14,13 @@ gulp.task('build-cli', function () {
     .pipe(plumber.stop())
     .pipe(changed(paths.output))
     .pipe(gulp.dest(paths.output))
-    .pipe(gulp.dest('/Users/joelcox1/Jobs/Aurelia/cli/cli-test/node_modules/aurelia-cli/dist'));
+    // .pipe(gulp.dest('/Users/joelcox1/Jobs/Aurelia/cli/cli-test/node_modules/aurelia-cli/dist'));
 });
 gulp.task('build-templates', function () {
   return gulp.src(paths.sourceTemp)
     .pipe(changed(paths.output))
     .pipe(gulp.dest(paths.output))
-    .pipe(gulp.dest('/Users/joelcox1/Jobs/Aurelia/cli/cli-test/node_modules/aurelia-cli/dist'));
+    // .pipe(gulp.dest('/Users/joelcox1/Jobs/Aurelia/cli/cli-test/node_modules/aurelia-cli/dist'));
 });
 
 gulp.task('build', function(callback) {
