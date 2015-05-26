9de672033534a391a71e4cae8505b17367510871
joelcoxokc <joel.cox.dev@gmail.com>
2015-05-25 21:42:44 -0700

    test(feat-new-commander): Create tests for new-commander

--- test/program.spce.js
+++ test/program.spce.js
@@ -24,24 +24,4 @@ describe('installer unit tests', () => {
     cmd     = argv.init._[0];
     command = sut.aurelia.commands[cmd];
   });
-
-  it('Should contain the correct command', (done) => {
-    expect(command.commandId).to.equal(cmd);
-    done();
-  });
-
-  it('Should emit the correct events', (done) => {
-    sut.program.on(cmd, function(){
-      expect(command.commandId).to.equal(cmd);
-      done();
-    });
-    sut.aurelia.run(argv.init);
-  });
-
-  describe('Command', function() {
-    it('Should have a commandId', function(done) {
-      expect(command.commandId).to.equal(cmd);
-      done();
-    });
-  });
 });
