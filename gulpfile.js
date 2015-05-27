7e41dd41e599ca4f86a7fa5a0bade5564bc26180
Zewa <zewa666@gmail.com>
2015-05-27 13:06:50 +0200

    fix(help): fixes help for generate command
    fixes the help text for the generate command, additionally cleansup utils and fixes gulpfile.

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,25 @@
 var gulp  = require('gulp');
 var babel = require('gulp-babel');
 
-var srcFiles      = ['src/**/*.js', '!src/**/templates/**/*.js']
+var srcFiles      = ['src/**/*.js', '!**/templates/**/*.js']
   , templateFiles = 'src/**/templates/**/*.js';
 
-gulp.task('default', function () {
-    return gulp.src(srcFiles)
-        .pipe(babel())
-        .pipe(gulp.src(templateFiles))
-        .pipe(gulp.dest('dist'));
+gulp.task('default', ['copy-source', 'copy-templates'], function () {
+
 });
 
-gulp.task('watch', ['default'], function() {
-  gulp.watch(srcFiles, ['default']);
+gulp.task('copy-source', function() {
+  return gulp.src(srcFiles)
+    .pipe(babel())
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('copy-templates', function() {
+  return gulp.src(templateFiles)
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('watch', function() {
+  gulp.watch(srcFiles, ['copy-source']);
+  gulp.watch(templateFiles, ['copy-templates']);
 });
