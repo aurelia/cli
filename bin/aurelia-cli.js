46189052bf693e255f94522e37f0ecf5d4054dc6
Rob Eisenberg <rob@bluespire.com>
2016-04-11 21:14:09 -0400

    feat(all): local vs. global cli

--- bin/aurelia-cli.js
+++ bin/aurelia-cli.js
@@ -1,11 +1,18 @@
 #!/usr/bin/env node
 "use strict";
-const CLI = require('../lib/cli').CLI;
-
 process.title = 'aurelia';
 
 let userArgs = process.argv.slice(2);
 let commandName = userArgs[0];
 let commandArgs = userArgs.slice(1);
+
+let CLI;
+
+try {
+  let location = require.resolve('aurelia-cli');
+  CLI = require(location).CLI;
+} catch (e) {
+  CLI = require('../lib/cli').CLI;
+}
 
 new CLI().run(commandName, commandArgs);
