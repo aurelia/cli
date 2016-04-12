0e9e8b07423b7b54fc96ef41b9437a02a8df0484
Rob Eisenberg <rob@bluespire.com>
2016-04-11 20:55:39 -0400

    chore(all): move non cli-specific code to lib folder

--- bin/aurelia-cli.js
+++ bin/aurelia-cli.js
@@ -1,22 +1,11 @@
 #!/usr/bin/env node
 "use strict";
+const CLI = require('../lib/cli').CLI;
+
+process.title = 'aurelia';
 
 let userArgs = process.argv.slice(2);
 let commandName = userArgs[0];
 let commandArgs = userArgs.slice(1);
 
-if (commandName) {
-  let Command;
-
-  try {
-    Command = require(`./commands/${commandName}/command`).Command;
-  } catch (e) {
-    console.log(`Invalid Command Name: ${commandName}`);
-    Command = require('./commands/help/command').Command;
-  }
-
-  new Command().execute(commandArgs);
-} else {
-  const HelpCommand = require('./commands/help/command').Command;
-  new HelpCommand().execute(commandArgs);
-}
+new CLI().run(commandName, commandArgs);
