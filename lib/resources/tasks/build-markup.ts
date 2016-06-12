283ab5355d1f6361e33fccb195a516db4dd76e05
Rob Eisenberg <rob@bluespire.com>
2016-06-12 18:44:36 +0200

    fix(build): clean old content structure

--- lib/resources/tasks/build-markup.ts
+++ lib/resources/tasks/build-markup.ts
@@ -1,7 +1,7 @@
 import * as gulp from 'gulp';
 import * as changedInPlace from 'gulp-changed-in-place';
 import * as project from '../aurelia.json';
-import {build} from 'aurelia/cli';
+import {build} from 'aurelia-cli';
 
 export default function buildHTML() {
   return gulp.src(project.paths.markup)
--- lib/resources/tasks/clean.ts
+++
@@ -1,6 +0,0 @@
-import * as del from 'del';
-import * as project from '../aurelia.json';
-
-export default function clean() {
-  return del([project.paths.output]);
-}
