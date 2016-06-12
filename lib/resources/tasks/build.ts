c3895f81bad3355c425f42468ab5fbeb88949c4a
Rob Eisenberg <rob@bluespire.com>
2016-06-12 18:50:40 +0200

    fix(build): correct import style

--- lib/resources/tasks/build.ts
+++ lib/resources/tasks/build.ts
@@ -2,7 +2,7 @@ import * as gulp from 'gulp';
 import buildStyles from './build-styles';
 import buildJavaScript from './build-javascript';
 import buildMarkup from './build-markup';
-import build from 'aurelia-cli';
+import {build} from 'aurelia-cli';
 import * as project from '../aurelia.json';
 
 export default gulp.series(
