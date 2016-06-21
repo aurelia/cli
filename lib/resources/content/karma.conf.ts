02c1af34bd26db45ea31e16237c0477f82844b93
Rob Eisenberg <rob@bluespire.com>
2016-06-21 09:09:29 -0400

    fix(karma): correct configuration

--- lib/resources/content/karma.conf.ts
+++ lib/resources/content/karma.conf.ts
@@ -10,7 +10,7 @@ let testSrc = [
 
 let output = project.platform.output;
 let appSrc = project.build.bundles.map(x => path.join(output, x.name));
-let entryIndex = appSrc.indexOf(path.join(output, project.build.loader.inject));
+let entryIndex = appSrc.indexOf(path.join(output, project.build.loader.configTarget));
 let entryBundle = appSrc.splice(entryIndex, 1)[0];
 let files = [entryBundle].concat(testSrc).concat(appSrc);
 
