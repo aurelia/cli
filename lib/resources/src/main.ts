6f963f72ca1c19bf94e029ea55b29573f8ab2d56
Rob Eisenberg <rob@bluespire.com>
2016-06-20 22:16:07 -0400

    feat(build): enable environment dependent deps and appends

--- lib/resources/src/main.ts
+++ lib/resources/src/main.ts
@@ -10,5 +10,9 @@ export function configure(aurelia: Aurelia) {
     aurelia.use.developmentLogging();
   }
 
+  if (environment.testing) {
+    aurelia.use.plugin('aurelia-testing');
+  }
+
   aurelia.start().then(() => aurelia.setRoot());
 }
