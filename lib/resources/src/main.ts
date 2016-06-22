4c5c8e14e7f69eae81e0b3df2887b26f4a076a2a
Rob Eisenberg <rob@bluespire.com>
2016-06-22 08:00:31 -0400

    fix(main): some bluebird and ts lib fixups

--- lib/resources/src/main.ts
+++ lib/resources/src/main.ts
@@ -1,6 +1,14 @@
 import {Aurelia} from 'aurelia-framework'
 import environment from './environment';
 
+//Configure Bluebird Promises.
+//Note: You may want to use environment-specific configuration.
+(<any>Promise).config({
+  warnings: {
+    wForgottenReturn: false
+  }
+});
+
 export function configure(aurelia: Aurelia) {
   aurelia.use
     .standardConfiguration()
