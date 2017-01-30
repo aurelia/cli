ada84bc47ac83ce199fc3e8b67b0601474ca48e2
Fabio Luz <fabio.luz@outlook.com>
2017-01-30 13:23:14 -0200

    fix(copy-files): copy files when building for production

--- lib/resources/tasks/copy-files.ts
+++ lib/resources/tasks/copy-files.ts
@@ -1,20 +1,48 @@
 import * as gulp from 'gulp';
 import * as path from 'path';
+import * as changedInPlace from 'gulp-changed-in-place';
 import * as project from '../aurelia.json';
 
 export default function copyFiles(done) {
-  const filesToCopy = project.build.copyFiles;
-  if (!filesToCopy) {
+  if (typeof project.build.copyFiles !== 'object') {
     done();
     return;
   }
- 
-  for (let file in filesToCopy) {
-    const source = path.posix.join(project.paths.root, file);
-    const target = path.posix.join(project.platform.output, filesToCopy[file]);
-    console.log(`Copying ${source} to ${target}`);
-    gulp.src(source).pipe(gulp.dest(target));
+
+  const instruction = getNormalizedInstruction();
+  const files = Object.keys(instruction);
+
+  return gulp.src(files)
+    .pipe(changedInPlace({ firstPass: true }))
+    .pipe(gulp.dest(x => {
+      const filePath = prepareFilePath(x.path);
+      const key = files.find(f => f === filePath);
+      return instruction[key];
+    }));
+}
+
+function getNormalizedInstruction() {
+  const files = project.build.copyFiles;
+  let normalizedFiles = {};
+
+  for (let key in files) {
+    normalizedFiles[path.posix.normalize(key)] = files[key];
   }
- 
-  done();
+
+  return normalizedFiles;
+}
+
+function prepareFilePath(filePath) {
+  let preparedPath = filePath.replace(process.cwd(), '');
+
+  //if we are running on windows we have to fix the path
+  if (/^win/.test(process.platform)) {
+    preparedPath = preparedPath.replace(/\\/g, '/');
+  }
+
+  if (preparedPath.startsWith('/')) {
+    preparedPath = preparedPath.substring(1);
+  }
+
+  return preparedPath;
 }
