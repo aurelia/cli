2cd3c56be5a43515f92d1a27f604eeadbb9e96f4
JeroenVinke <jeroenvinke17@gmail.com>
2017-03-29 21:28:23 +0200

    test(bundle): added test coverage for Bundle

---
+++ spec/mocks/bundler.js
@@ -0,0 +1,8 @@
+module.exports = class Bundler {
+  constructor() {
+    this.itemIncludedInBuild = jasmine.createSpy('itemIncludedInBuild');
+    this.interpretBuildOptions = jasmine.createSpy('interpretBuildOptions');
+    this.configureDependency = jasmine.createSpy('configureDependency');
+    this.addFile = jasmine.createSpy('addFile');
+  }
+};
