2c08625d44910a6f2e044fffc3e9ff0f4f93e45c
JeroenVinke <jeroenvinke17@gmail.com>
2017-07-14 21:10:08 +0200

    feat(all): add webpack
    closes https://github.com/aurelia/cli/issues/184
    closes https://github.com/aurelia/cli/issues/260
    
    chore(webpack): clean up tasks, update command json
    
    chore(webpack): cleanup
    
    chore(new): ask for loader/bundler in custo
    
    chore(webpack): add asp.net core support
    
    chore(webpack): replace existing csproj if present
    
    fix(project-item): ask for file replacement in sequence
    
    in "au new" the CLI asks the user to replace existing file. When there are multiple files that already exist, the CLI only asks the user whether to replace the file for one of those files. This is due to the fact that the .create() method of child project-item's are called in parallel, not in sequence.
    
    chore(webpack): improve instructions for aspnet core
    
    chore(all): fixed minor scaffolding issues
    
    chore(webpack): more user information
    
    chore(webpack): add tasks for karma & jest

--- lib/resources/tasks/run-webpack.ts
+++ lib/resources/tasks/run-webpack.ts
@@ -1,34 +1,22 @@
 import {config} from './build';
 import * as webpack from 'webpack';
 import * as Server from 'webpack-dev-server';
-import * as open from 'opn';
-import * as yargs from 'yargs';
-import * as url from 'url';
 import * as project from '../aurelia.json';
-import {CLIOptions} from 'aurelia-cli';
-
-const argv = yargs.argv;
-argv.color = require('supports-color');
+import {CLIOptions, reportWebpackReadiness} from 'aurelia-cli';
 
 function run(done) {
+  // https://webpack.github.io/docs/webpack-dev-server.html
   let opts = {
     host: 'localhost',
     publicPath: config.output.publicPath,
     filename: config.output.filename,
-    watchOptions: undefined,
-    hot: project.platform.hmr,
-    clientLogLevel: 'info',
+    hot: project.platform.hmr || CLIOptions.hasFlag('hmr'),
     port: project.platform.port,
-
     contentBase: config.output.path,
-    // serve index.html for all 404 (required for push-state)
     historyApiFallback: true,
-
     open: project.platform.open,
     stats: {
-      cached: false,
-      cachedAssets: false,
-      colors: argv.color
+      colors: require('supports-color')
     }
   };
 
@@ -36,7 +24,7 @@ function run(done) {
     opts.watch = false;
   }
 
-  if (project.platform.hmr) {
+  if (project.platform.hmr || CLIOptions.hasFlag('hmr')) {
     config.plugins.push(new webpack.HotModuleReplacementPlugin());
     config.entry.app.unshift(`webpack-dev-server/client?http://${opts.host}:${opts.port}/`, 'webpack/hot/dev-server');
   }
@@ -46,56 +34,9 @@ function run(done) {
 
   server.listen(opts.port, opts.host, function(err) {
     if (err) throw err;
-    reportReadiness(createDomain(opts), opts);
+    reportWebpackReadiness(opts);
     done();
   });
-}
-
-function reportReadiness(uri, options) {
-  const useColor = argv.color;
-
-  let startSentence = `Project is running at ${colorInfo(useColor, uri)}`;
-
-  if (options.socket) {
-    startSentence = `Listening to socket at ${colorInfo(useColor, options.socket)}`;
-  }
-  console.log((argv.progress ? '\n' : '') + startSentence);
-
-  console.log(`webpack output is served from ${colorInfo(useColor, options.publicPath)}`);
-  const contentBase = Array.isArray(options.contentBase) ? options.contentBase.join(', ') : options.contentBase;
-
-  if (contentBase) {
-    console.log(`Content not from webpack is served from ${colorInfo(useColor, contentBase)}`);
-  }
-
-  if (options.historyApiFallback) {
-    console.log(`404s will fallback to ${colorInfo(useColor, options.historyApiFallback.index || '/index.html')}`);
-  }
-
-  if (options.open) {
-    open(uri).catch(function() {
-      console.log('Unable to open browser. If you are running in a headless environment, please do not use the open flag.');
-    });
-  }
-}
-
-function createDomain(opts) {
-  const protocol = opts.https ? 'https' : 'http';
-
-  // the formatted domain (url without path) of the webpack server
-  return opts.public ? `${protocol}://${opts.public}` : url.format({
-    protocol: protocol,
-    hostname: opts.host,
-    port: opts.socket ? 0 : opts.port.toString()
-  });
-}
-
-function colorInfo(useColor, msg) {
-  if (useColor) {
-    // Make text blue and bold, so it *pops*
-    return `\u001b[1m\u001b[33m${msg}\u001b[39m\u001b[22m`;
-  }
-  return msg;
 }
 
 export { run as default };
