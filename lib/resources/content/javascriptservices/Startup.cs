c83078476ab63f55410a6bbc3abd00e43373ed84
Gareth Barlow <gareth@nbninternational.com>
2017-09-02 21:20:39 +0100

    fix #734, #701 - aspnet core 2.0 template

--- lib/resources/content/javascriptservices/Startup.cs
+++ lib/resources/content/javascriptservices/Startup.cs
@@ -13,17 +13,13 @@ namespace WebApplicationBasic
 {
     public class Startup
     {
-        public Startup(IHostingEnvironment env)
+        
+        public Startup(IConfiguration configuration)
         {
-            var builder = new ConfigurationBuilder()
-                .SetBasePath(env.ContentRootPath)
-                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
-                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
-                .AddEnvironmentVariables();
-            Configuration = builder.Build();
+            Configuration = configuration;
         }
 
-        public IConfigurationRoot Configuration { get; }
+        public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
@@ -35,9 +31,6 @@ namespace WebApplicationBasic
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
-            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
-            loggerFactory.AddDebug();
-
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
