84a70bdc4a1908b0b26d8479fb1a0bb57018d9a1
Jan-Patrick Ahnen <jan.ahnen@gmail.com>
2018-03-07 17:24:44 +0100

    prevent error output to console on failed tests

--- lib/resources/tasks/test.ts
+++ lib/resources/tasks/test.ts
@@ -9,7 +9,7 @@ let karma = done => {
   new Karma({
     configFile: path.join(__dirname, '/../../karma.conf.js'),
     singleRun: !CLIOptions.hasFlag('watch')
-  }, done).start();
+  }, () => { done(); }).start();
 };
 
 let unit;
