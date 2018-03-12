4ad96fda14d3606711ea037de0c9c9948950474b
Alexander Taran <a.taran@outlook.com>
2018-03-12 03:19:03 +0300

    fix(stylus): don't stop watch on error

--- lib/resources/tasks/process-stylus.ts
+++ lib/resources/tasks/process-stylus.ts
@@ -2,11 +2,14 @@ import * as gulp from 'gulp';
 import * as changedInPlace from 'gulp-changed-in-place';
 import * as sourcemaps from 'gulp-sourcemaps';
 import * as stylus from 'gulp-stylus';
+import * as plumber from 'gulp-plumber';
+import * as notify from 'gulp-notify';
 import * as project from '../aurelia.json';
 import {build} from 'aurelia-cli';
 
 export default function processCSS() {
   return gulp.src(project.cssProcessor.source)
+    .pipe(plumber({ errorHandler: notify.onError('Error: <%= error.message %>') }))
     .pipe(changedInPlace({firstPass:true}))
     .pipe(sourcemaps.init())
     .pipe(stylus())
