c706c817d5841284004b43bbb234fef5cdeb4f2b
Chunpeng Huo <huocp@me.com>
2018-08-18 22:55:00 +1000

    fix: fix environment check (--env or NODE_ENV)
    Check env (either from --env or NODE_ENV) against user defined env files (aurelia_project/environments/*).
    Normalize Node.js NODE_ENV convention production/development to prod/dev, but only if user didn't define production.js or development.js.
    Stop the process immediately if env file is missing.
    
    closes #912

--- spec/lib/cli-options.spec.js
+++ spec/lib/cli-options.spec.js
@@ -1,9 +1,23 @@
 'use strict';
 describe('The cli-options', () => {
   let cliOptions;
+  let mockfs;
 
   beforeEach(() => {
+    mockfs = require('mock-fs');
+    const fsConfig = {
+      'aurelia_project/environments/dev.js': 'content',
+      'aurelia_project/environments/stage.js': 'content',
+      'aurelia_project/environments/prod.js': 'content'
+    };
+    mockfs(fsConfig);
+
     cliOptions = new(require('../../lib/cli-options').CLIOptions)();
+  });
+
+  afterEach(() => {
+    delete process.env.NODE_ENV;
+    mockfs.restore();
   });
 
   describe('The CLIOptions', () => {
@@ -22,6 +36,108 @@ describe('The cli-options', () => {
           expect(cliOptions.taskName()).toBe(files[file]);
         }
       }
+    });
+
+    it('gets env from arg --env', () => {
+      cliOptions.args = ['build', '--env', 'prod'];
+      expect(cliOptions.getEnvironment()).toBe('prod');
+    });
+
+    it('gets env from NODE_ENV', () => {
+      process.env.NODE_ENV = 'dev';
+      cliOptions.args = ['build'];
+      expect(cliOptions.getEnvironment()).toBe('dev');
+    });
+
+    it('normalizes env from production to prod', () => {
+      cliOptions.args = ['build', '--env', 'production'];
+      expect(cliOptions.getEnvironment()).toBe('prod');
+    });
+
+    it('does not normalizes env from production to prod if production.js is defined', () => {
+      const fsConfig = {
+        'aurelia_project/environments/development.js': 'content',
+        'aurelia_project/environments/stage.js': 'content',
+        'aurelia_project/environments/production.js': 'content'
+      };
+      mockfs(fsConfig);
+      cliOptions.args = ['build', '--env', 'production'];
+      expect(cliOptions.getEnvironment()).toBe('production');
+    });
+
+    it('normalizes env from development to dev', () => {
+      cliOptions.args = ['build', '--env', 'development'];
+      expect(cliOptions.getEnvironment()).toBe('dev');
+    });
+
+    it('does not normalizes env from development to dev if development.js is defined', () => {
+      const fsConfig = {
+        'aurelia_project/environments/development.js': 'content',
+        'aurelia_project/environments/stage.js': 'content',
+        'aurelia_project/environments/production.js': 'content'
+      };
+      mockfs(fsConfig);
+      cliOptions.args = ['build', '--env', 'development'];
+      expect(cliOptions.getEnvironment()).toBe('development');
+    });
+
+    it('terminates when env is not defined by an env file', () => {
+      let oldExit = process.exit;
+      let spy = jasmine.createSpy('exit');
+      process.exit = spy;
+
+      cliOptions.args = ['build', '--env', 'unknown'];
+      cliOptions.getEnvironment();
+      expect(spy).toHaveBeenCalledWith(1);
+      process.exit = oldExit;
+    });
+
+    it('normalizes NODE_ENV from production to prod', () => {
+      process.env.NODE_ENV = 'production';
+      cliOptions.args = ['build'];
+      expect(cliOptions.getEnvironment()).toBe('prod');
+    });
+
+    it('does not normalizes NODE_ENV from production to prod if production.js is defined', () => {
+      const fsConfig = {
+        'aurelia_project/environments/development.js': 'content',
+        'aurelia_project/environments/stage.js': 'content',
+        'aurelia_project/environments/production.js': 'content'
+      };
+      mockfs(fsConfig);
+      process.env.NODE_ENV = 'production';
+      cliOptions.args = ['build'];
+      expect(cliOptions.getEnvironment()).toBe('production');
+    });
+
+    it('normalizes NODE_ENV from development to dev', () => {
+      process.env.NODE_ENV = 'development';
+      cliOptions.args = ['build'];
+      expect(cliOptions.getEnvironment()).toBe('dev');
+    });
+
+    it('does not normalizes env from development to dev if development.js is defined', () => {
+      const fsConfig = {
+        'aurelia_project/environments/development.js': 'content',
+        'aurelia_project/environments/stage.js': 'content',
+        'aurelia_project/environments/production.js': 'content'
+      };
+      mockfs(fsConfig);
+      process.env.NODE_ENV = 'development';
+      cliOptions.args = ['build'];
+      expect(cliOptions.getEnvironment()).toBe('development');
+    });
+
+    it('terminates when NODE_ENV is not defined by an env file', () => {
+      let oldExit = process.exit;
+      let spy = jasmine.createSpy('exit');
+      process.exit = spy;
+
+      process.env.NODE_ENV = 'unknown';
+      cliOptions.args = ['build'];
+      cliOptions.getEnvironment();
+      expect(spy).toHaveBeenCalledWith(1);
+      process.exit = oldExit;
     });
   });
 });
