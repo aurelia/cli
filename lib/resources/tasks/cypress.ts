ea43b597507763ed5ad077c9d4bd642dc42372c6
Michael Withagen <michael.withagen@live.com>
2019-02-20 19:06:27 +0100

    fix(cypress task): Return error when tests fail
    Change resolve to cb to follow gulp standards.

--- lib/resources/tasks/cypress.ts
+++ lib/resources/tasks/cypress.ts
@@ -2,12 +2,12 @@ import * as cypress from 'cypress';
 import { CLIOptions } from 'aurelia-cli';
 import * as config from '../../cypress.config';
 
-export default (resolve) => {
+export default (cb) => {
   if (CLIOptions.hasFlag('run')) {
     cypress
       .run(config)
-      .then(results => (results.totalFailed === 0 ? resolve() : resolve('Run failed!')))
-      .catch(resolve);
+      .then(results => (results.totalFailed === 0 ? cb() : cb('Run failed!')))
+      .catch(cb);
   } else {
     cypress.open(config);
   }
