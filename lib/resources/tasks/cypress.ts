7fa7e9e5db16be51a38aa9b42eecfcbac93230e4
Michael Withagen <michael.withagen@live.com>
2019-02-20 18:37:19 +0100

    fix(cypress task): Return error when tests fail
    Make au cypress --run emit error when a test failed to allow a build
    process/step to fail.
    
    Closes #1057

--- lib/resources/tasks/cypress.ts
+++ lib/resources/tasks/cypress.ts
@@ -2,9 +2,12 @@ import * as cypress from 'cypress';
 import { CLIOptions } from 'aurelia-cli';
 import * as config from '../../cypress.config';
 
-export default () => {
+export default (resolve) => {
   if (CLIOptions.hasFlag('run')) {
-    cypress.run(config);
+    cypress
+      .run(config)
+      .then(results => (results.totalFailed === 0 ? resolve() : resolve('Run failed!')))
+      .catch(resolve);
   } else {
     cypress.open(config);
   }
