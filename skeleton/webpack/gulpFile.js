2e728aa69fe8dfcfdb740b8c6dc56dbbf6b44e25
Sayan751 <sayan751@gmail.com>
2019-05-19 21:21:17 +0200

    fixed the gulp file

--- skeleton/webpack/gulpFile.js
+++ skeleton/webpack/gulpFile.js
@@ -1,17 +1,10 @@
-// @if feat.babel
-import gulp from 'gulp';
-import del from 'del';
-// @endif
-// @if feat.typescript
-import * as gulp from 'gulp';
-import * as del from 'del';
-// @endif
+const del = require('del');
+const gulp = require('gulp');
+const path = require('path');
+const project = require('./aurelia_project/aurelia.json');
 
 function clearDist() {
-  return del([config.output.path]);
+  return del([path.resolve(__dirname, project.platform.output)]);
 }
 
-gulp.task("prebuild", gulp.series(
-  clearDist,
-  configureEnvironment
-));
+gulp.task("prebuild", clearDist);
