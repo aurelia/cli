3dccfb0f37fe8ad1dc88ab10e9e9c9b2e5aa76bd
Saeed Ganji <shahabganji@gmail.com>
2019-08-16 13:28:24 +0430

    feat(protractor): update protractor to reflect changes for running with custom port & host
    Also update cypress.config.js, this one missed in the previous commit

--- skeleton/cypress/cypress.config.js
+++ skeleton/cypress/cypress.config.js
@@ -1,9 +1,11 @@
+const CLIOptions =  require( 'aurelia-cli').CLIOptions;
 const aureliaConfig = require('./aurelia_project/aurelia.json');
-const port = aureliaConfig.platform.port;
+const PORT = CLIOptions.getFlagValue('port') || aureliaConfig.platform.port;
+const HOST = CLIOptions.getFlagValue('host') || aureliaConfig.platform.host;
 
 module.exports = {
   config: {
-    baseUrl: `http://localhost:${port}`,
+    baseUrl: `http://${HOST}:${PORT}`,
     fixturesFolder: 'test/e2e/fixtures',
     integrationFolder: 'test/e2e/integration',
     pluginsFile: 'test/e2e/plugins/index.js',
