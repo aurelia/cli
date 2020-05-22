ed4dbc62022245c7f4cc1d097c50f3db70f3e33b
Chunpeng Huo <huocp@me.com>
2020-05-22 13:34:09 +1000

    chore: clean up gulpfile

--- build/tasks/lint.js
+++
@@ -1,9 +0,0 @@
-const gulp = require('gulp');
-const eslint = require('gulp-eslint');
-
-gulp.task('lint', function() {
-  return gulp.src(['lib/**/*.js', 'spec/**/*.js', 'lib/**/*.ts', 'spec/**/*.ts'])
-    .pipe(eslint())
-    .pipe(eslint.format())
-    .pipe(eslint.failOnError());
-});
--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,40 @@
-'use strict';
-require('./build/tasks/lint.js');
-require('./build/tasks/prepare-release.js');
+const gulp = require('gulp');
+const eslint = require('gulp-eslint');
+const conventionalChangelog = require('gulp-conventional-changelog');
+const bump = require('gulp-bump');
+const yargs = require('yargs');
+
+function lint() {
+  return gulp.src(['lib/**/*.js', 'spec/**/*.js', 'lib/**/*.ts', 'spec/**/*.ts'])
+    .pipe(eslint())
+    .pipe(eslint.format())
+    .pipe(eslint.failOnError());
+}
+
+function bumpVersion() {
+  const validBumpTypes = ['major', 'minor', 'patch', 'prerelease'];
+  const bumpType = (yargs.argv.bump || 'patch').toLowerCase();
+
+  if (validBumpTypes.indexOf(bumpType) === -1) {
+    throw new Error('Unrecognized bump "' + bumpType + '".');
+  }
+
+  return gulp.src('package.json')
+    .pipe(bump({type: bumpType})) //major|minor|patch|prerelease
+    .pipe(gulp.dest('./'));
+}
+
+function changelog() {
+  return gulp.src('doc/CHANGELOG.md')
+    .pipe(conventionalChangelog({preset: 'angular'}))
+    .pipe(gulp.dest('doc'));
+}
+
+exports.lint = lint;
+exports['bump-version'] = bumpVersion;
+exports.changelog = changelog;
+exports['prepare-release'] = gulp.series(
+  lint,
+  bumpVersion,
+  changelog
+);
