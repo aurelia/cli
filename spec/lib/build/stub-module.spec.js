9bae0692857f778bcd2b860c0107a706b74ad17b
Chunpeng Huo <huocp@me.com>
2020-05-28 16:53:19 +1000

    chore(cli-bundler): switch to querystring-browser-stub and fs-browser-stub

--- spec/lib/build/stub-module.spec.js
+++ spec/lib/build/stub-module.spec.js
@@ -8,11 +8,50 @@ describe('StubCoreNodejsModule', () => {
     });
   });
 
+  it('stubs domain', () => {
+    expect(stubModule('domain', 'src')).toEqual({
+      name: 'domain',
+      path: '../node_modules/domain-browser'
+    });
+  });
+
+  it('stubs http', () => {
+    expect(stubModule('http', 'src')).toEqual({
+      name: 'http',
+      path: '../node_modules/stream-http'
+    });
+  });
+
+  it('stubs querystring', () => {
+    expect(stubModule('querystring', 'src')).toEqual({
+      name: 'querystring',
+      path: '../node_modules/querystring-browser-stub'
+    });
+  });
+
+  it('stubs fs', () => {
+    expect(stubModule('fs', 'src')).toEqual({
+      name: 'fs',
+      path: '../node_modules/fs-browser-stub'
+    });
+  });
+
   it('ignores sys', () => {
     expect(stubModule('sys', 'src')).toBeUndefined();
   });
 
+  it('stubModule stubs zlib', () => {
+    expect(stubModule('zlib', 'src')).toEqual({
+      name: 'zlib',
+      path: '../node_modules/browserify-zlib'
+    });
+  });
+
   it('stubs empty module for some core module', () => {
-    expect(stubModule('fs', 'src')).toBe('define(function(){});');
+    expect(stubModule('dns', 'src')).toBe('define(function(){});');
+  });
+
+  it('stubs empty module for __ignore__', () => {
+    expect(stubModule('__ignore__', 'src')).toBe('define(function(){});');
   });
 });
