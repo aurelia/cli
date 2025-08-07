9bc191a2ac460bffd585dc6abd9f4db592181233
Nenad Vićentić <nenadvicentic@gmail.com>
2025-08-07 21:12:39 +0200

    Remove: obsolete @stylistic/eslint-plugin-js npm package

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,7 +3,6 @@
 import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
 import stylistic from '@stylistic/eslint-plugin';
-import stylisticJS from '@stylistic/eslint-plugin-js';
 
 export default tseslint.config(
   {
@@ -19,8 +18,7 @@ export default tseslint.config(
       }
     },
     plugins: {
-      '@stylistic': stylistic,
-      '@stylistic/js': stylisticJS
+      '@stylistic': stylistic
     }
   },
   {
@@ -38,7 +36,7 @@ export default tseslint.config(
       }],
       'prefer-rest-params': 'warn',
       'prefer-spread': 'warn',
-      '@stylistic/js/quotes': ['warn', 'single'],
+      '@stylistic/quotes': ['warn', 'single'],
       '@typescript-eslint/no-explicit-any': 'warn',
       '@typescript-eslint/no-require-imports': 'warn',
       '@typescript-eslint/no-floating-promises': 'error',
